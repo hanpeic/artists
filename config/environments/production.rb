635a5c00b77e438271e5794fa2ee7cfa53f01a86
Pei Han <hanpeic@gmail.com>
2017-09-18 20:38:34 +1000

    remove unuse code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
@@ -65,5 +65,5 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  #config.active_record.dump_schema_after_migration = false
 end
